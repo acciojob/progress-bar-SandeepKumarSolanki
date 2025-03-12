ab0f751e091f1fbaf55b70b4d2ce37eee8e5bfa7
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-03-12 12:34:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+
+document.addEventListener("DOMContentLoaded" , ()=> {
+	let prevBtn = document.getElementById("prev");
+	let nextBtn = document.getElementById("next");
+	const progressCircle = document.querySelectorAll('.circle-container')
+	progressCircle.forEach((cir , index) => {
+		nextBtn.addEventListener("click" , ()=>{
+			if (cir.value && index < cir.length - 1) {
+				cir.style.backgroundColor = "wheat";
+                // Focus on next input field after typing
+                cir[index + 1].focus();
+            }
+		})
+	})
+})
